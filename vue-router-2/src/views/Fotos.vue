d6146faaedcc18b94142362269993c84f20c8443
Jhersy <jhersyvb@gmail.com>
2020-03-29 19:28:30 -0500

    #20 Parametros dinamicos y rutas a traves de eventos | Curso de Vue.js Desde Cero

--- vue-router-2/src/views/Fotos.vue
+++ vue-router-2/src/views/Fotos.vue
@@ -1,7 +1,20 @@
 <template>
   <div>
     <h1>Galería de fotos</h1>
+
+    <router-link
+      v-for="(item, index) in fotos"
+      :key="index"
+      :to="{ name: 'Fotos', params: { id: item } }"
+    >
+      <button>Foto {{ item }}</button>
+    </router-link>
+
     <Foto />
+
+    <button @click="home">Home</button>
+    <button @click="anterior">Anterior</button>
+    <button @click="siguiente">Siguiente</button>
   </div>
 </template>
 
@@ -9,6 +22,26 @@
   import Foto from '@/components/Fotografia.vue'
 
   export default {
-    components: { Foto }
+    components: { Foto },
+
+    data() {
+      return {
+        fotos: [1, 2, 3]
+      }
+    },
+
+    methods: {
+      home() {
+        this.$router.push('/')
+      },
+
+      anterior() {
+        this.$router.go(-1)
+      },
+
+      siguiente() {
+        this.$router.go(1)
+      }
+    }
   }
 </script>
