4a75f893e99cf3c7bc2a83a98dc4eb31bc859da9
Jhersy <jhersyvb@gmail.com>
2020-03-31 10:17:19 -0500

    Vuetify, Axios y Vuex [Práctica] - Grid en Vuetify, creando cards y vista calendario

--- axios-dolar/src/views/Home.vue
+++ axios-dolar/src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-layout wrap>
+    <v-flex xs12>
+      <v-card>
+        <v-date-picker v-model="fecha" full-width></v-date-picker>
+      </v-card>
+      <v-card color="error" dark>
+        <v-card-text class="display-1 text-center">
+          650 - {{ fecha }}
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+
+  data() {
+    return {
+      fecha: ''
+    }
   }
 }
 </script>
