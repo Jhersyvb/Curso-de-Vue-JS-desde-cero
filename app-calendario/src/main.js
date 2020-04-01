1e30b93fda8d292d029f7a1daeec8697b306e4e3
Jhersy <jhersyvb@gmail.com>
2020-04-01 10:49:33 -0500

    Vue JS + Firebase + Vuetify: Configuracion de main.js

--- app-calendario/src/main.js
+++ app-calendario/src/main.js
@@ -4,6 +4,18 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 
+import VueTextareaAutosize from 'vue-textarea-autosize'
+Vue.use(VueTextareaAutosize)
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import firebaseConfig from '../firebase.config'
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const db = firebase.firestore()
+
 Vue.config.productionTip = false
 
 new Vue({
