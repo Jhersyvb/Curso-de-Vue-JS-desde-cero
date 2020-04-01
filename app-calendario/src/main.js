a8aa2eb56085abd52ea21387bfb4bd462b255b4c
Jhersy <jhersyvb@gmail.com>
2020-04-01 11:00:44 -0500

    Vue JS + Firebase + Vuetify: Agregar Calendario.vue y configurar App.vue

--- app-calendario/src/main.js
+++ app-calendario/src/main.js
@@ -18,6 +18,16 @@ export const db = firebase.firestore()
 
 Vue.config.productionTip = false
 
+const ignoreWarnMessage = 'The .native modifier for v-on is only valid on components but it was used on <div>.';
+Vue.config.warnHandler = function (msg, vm, trace) {
+  // `trace` is the component hierarchy trace
+  if (msg === ignoreWarnMessage) {
+    msg = null;
+    vm = null;
+    trace = null;
+  }
+}
+
 new Vue({
   router,
   store,
