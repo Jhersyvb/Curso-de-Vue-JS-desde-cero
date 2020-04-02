1c58ee0bae661f674c55cfedffffe0fd8c8c9b44
Jhersy <jhersyvb@gmail.com>
2020-04-01 19:17:16 -0500

    NUXT JS ⚡ NuxtLink

--- nuxt-js-app/pages/blog/index.vue
+++ nuxt-js-app/pages/blog/index.vue
@@ -1,21 +1,39 @@
 <template>
   <div class="container">
-    <ul>
-      <li>Artículo #1</li>
-      <li>Artículo #2</li>
-      <li>Artículo #3</li>
-      <li>Artículo #4</li>
-      <li>Artículo #5</li>
-    </ul>
+    <h1 class="text-center">Artículos</h1>
+    <div class="row">
+      <div
+        v-for="articulo in articulos"
+        :key="articulo.id"
+        class="col-md-4 my-2"
+      >
+        <div class="card">
+          <div class="card-body">
+            <div class="card-text">
+              <h2>
+                <nuxt-link :to="`/blog/${articulo.id}`">
+                  {{ articulo.titulo }}
+                </nuxt-link>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      articulos: [
+        { id: 1, titulo: 'Título #1' },
+        { id: 2, titulo: 'Título #2' },
+        { id: 3, titulo: 'Título #3' },
+        { id: 4, titulo: 'Título #4' }
+      ]
+    }
+  }
 }
 </script>
-
-<style>
-
-</style>
