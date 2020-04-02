f4c6244a11ac1381dd9e74d96d3a6e1490b9c20e
Jhersy <jhersyvb@gmail.com>
2020-04-01 19:31:33 -0500

    NUXT JS ⚡ Layout personalizado

---
+++ nuxt-js-app/layouts/nosotros.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav class="border-right float-left p-3">
+      <ul class="nav flex-column">
+        <li class="nav-item border-bottom">
+          <a class="nav-link" href="#">Quienes somos</a>
+        </li>
+        <li class="nav-item border-bottom">
+          <a class="nav-link" href="#">Equipo</a>
+        </li>
+        <li class="nav-item border-bottom">
+          <a class="nav-link" href="#">Misión</a>
+        </li>
+        <li class="nav-item border-bottom">
+          <a class="nav-link" href="#">Visión</a>
+        </li>
+      </ul>
+    </nav>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- nuxt-js-app/pages/nosotros.vue
+++ nuxt-js-app/pages/nosotros.vue
@@ -6,6 +6,6 @@
 
 <script>
 export default {
-
+  layout: 'nosotros'
 }
 </script>
